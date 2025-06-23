70969e7cccb906e8e320151f47832c1f5429073d
Ismail1605 <135840607+Ismail1605@users.noreply.github.com>
2025-06-23 09:35:46 +0100

    Add files via upload

---
+++ salles_data.py
@@ -0,0 +1,114 @@
+salles_data = [
+    {
+        "Nom":"ASS_ES-SALAAM OUED ZEM",
+        "Code":"C058-1",
+        "Tarif":540,
+        "Catégorie":"H & F",
+        "Discipline":"MUSC FITNESS + CARDIO + AÉROBIC"
+    },
+    {
+        "Nom":"ASS_ES-SALAAM OUED ZEM",
+        "Code":"C058-2",
+        "Tarif":450,
+        "Catégorie":"E",
+        "Discipline":"MUSC FITNESS + CARDIO + AÉROBIC"
+    },
+    {
+        "Nom":"ASS_JAMALI OUED ZEM",
+        "Code":"C059-1",
+        "Tarif":450,
+        "Catégorie":"H & F",
+        "Discipline":"MUSC - CARDIO"
+    },
+    {
+        "Nom":"ASS_JAMALI OUED ZEM",
+        "Code":"C059-2",
+        "Tarif":360,
+        "Catégorie":"E",
+        "Discipline":"MUSC - CARDIO"
+    },
+    {
+        "Nom":"ASS_EL ANDALOUS OUED ZEM",
+        "Code":"C060-1",
+        "Tarif":450,
+        "Catégorie":"H & F",
+        "Discipline":"MUSCULATION"
+    },
+    {
+        "Nom":"ASS_EL ANDALOUS OUED ZEM",
+        "Code":"C060-2",
+        "Tarif":360,
+        "Catégorie":"E",
+        "Discipline":"MUSCULATION"
+    },
+    {
+        "Nom":"ASS_EL HAMRA",
+        "Code":"C028-1",
+        "Tarif":630,
+        "Catégorie":"H & F",
+        "Discipline":"KARATE + FULL-CONTACT"
+    },
+    {
+        "Nom":"ASS_EL HAMRA",
+        "Code":"C028-2",
+        "Tarif":480,
+        "Catégorie":"E",
+        "Discipline":"KARATE + FULL-CONTACT"
+    },
+    {
+        "Nom":"ASS_RIMAI DAHABA",
+        "Code":"C026-1",
+        "Tarif":525,
+        "Catégorie":"H & F",
+        "Discipline":"AÉROBIC"
+    },
+    {
+        "Nom":"ASS_RIMAI DAHABA",
+        "Code":"C026-2",
+        "Tarif":420,
+        "Catégorie":"E",
+        "Discipline":"AÉROBIC"
+    },
+    {
+        "Nom":"ASS_ELITE ARTS",
+        "Code":"C072-2",
+        "Tarif":330,
+        "Catégorie":"E",
+        "Discipline":"TAEKWONDO + ARTS MARTIAUX"
+    },
+    {
+        "Nom":"ASS_TOTAL FITNESS",
+        "Code":"C050-2",
+        "Tarif":610,
+        "Catégorie":"H & F",
+        "Discipline":"MUSCULATION + KICKBOXING"
+    },
+    {
+        "Nom":"ASS_POWER FITNESS",
+        "Code":"C004-1",
+        "Tarif":530,
+        "Catégorie":"H & F",
+        "Discipline":"TAEKWONDO + KARATÉ"
+    },
+    {
+        "Nom":"ASS_OLYMPIQUE KHOURIBGA",
+        "Code":"C019-2",
+        "Tarif":345,
+        "Catégorie":"E",
+        "Discipline":"KARATE CONTACT + TAEKWONDO"
+    },
+    {
+        "Nom":"ASS_FADET YASSMINA",
+        "Code":"C003-2",
+        "Tarif":270,
+        "Catégorie":"E",
+        "Discipline":"GYM-KIDDO + FULLCONTACT"
+    },
+    {
+        "Nom":"ASS_CHOTOKANE HATTANE",
+        "Code":"C066-1",
+        "Tarif":355,
+        "Catégorie":"H & F",
+        "Discipline":"KARATE ET MUSCULATION"
+    }
+]
